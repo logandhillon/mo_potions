6758f0a45716450956890e8df0ddeea0f587960a
Logan Dhillon <dev@logandhillon.com>
2026-07-08 23:37:40 -0400

    chore: remove angel totem

--- src/main/java/net/ldm/mopotions/init/MoPotionsModItems.java
+++ src/main/java/net/ldm/mopotions/init/MoPotionsModItems.java
@@ -7,13 +7,11 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraft.world.item.Item;
 
 import net.ldm.mopotions.item.FermentedSugarItem;
-import net.ldm.mopotions.item.AngelTotemItem;
 import net.ldm.mopotions.MoPotionsMod;
 
 public class MoPotionsModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, MoPotionsMod.MOD_ID);
 	public static final RegistryObject<Item> FERMENTED_SUGAR = REGISTRY.register("fermented_sugar", FermentedSugarItem::new);
-	public static final RegistryObject<Item> ANGEL_TOTEM = REGISTRY.register("angel_totem", AngelTotemItem::new);
 	//public static final RegistryObject<Item> POTION_INFUSER = block(MoPotionsModBlocks.POTION_INFUSER, CreativeModeTab.TAB_BREWING);
 
 //	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
