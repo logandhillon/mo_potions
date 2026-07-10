92be7c9fef0aca607ab5d1e3179a24b0190aca11
Logan Dhillon <dev@logandhillon.com>
2026-07-10 00:11:44 -0400

    feat(datagen): generate translations for potions

--- src/main/java/net/ldm/mopotions/datagen/LangProvider.java
+++ src/main/java/net/ldm/mopotions/datagen/LangProvider.java
@@ -1,6 +1,7 @@
 package net.ldm.mopotions.datagen;
 
 import net.ldm.mopotions.MoPotionsMod;
+import net.ldm.mopotions.init.MoPotionsPotions;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.LanguageProvider;
 
@@ -14,6 +15,13 @@ public class LangProvider extends LanguageProvider {
 
     @Override
     protected void addTranslations() {
-
+        for (Translation<String> translation: MoPotionsPotions.TRANSLATIONS) {
+            add("item.minecraft.potion.effect."+translation.ref, String.format("Potion of %s", translation.name));
+            add("item.minecraft.splash_potion.effect."+translation.ref, String.format("Splash Potion of %s", translation.name));
+            add("item.minecraft.lingering_potion.effect."+translation.ref, String.format("Lingering Potion of %s", translation.name));
+            add("item.minecraft.tipped_arrow.effect."+translation.ref, String.format("Arrow of %s", translation.name));
+        }
     }
+
+    public record Translation<T>(T ref, String name) {}
 }
--- src/main/java/net/ldm/mopotions/init/MoPotionsPotions.java
+++ src/main/java/net/ldm/mopotions/init/MoPotionsPotions.java
@@ -1,6 +1,8 @@
 package net.ldm.mopotions.init;
 
 import net.ldm.mopotions.MoPotionsMod;
+import net.ldm.mopotions.datagen.LangProvider;
+import net.ldm.mopotions.datagen.LangProvider.Translation;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.alchemy.Potion;
@@ -8,129 +10,142 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.ArrayList;
+import java.util.function.Supplier;
+
 public class MoPotionsPotions {
     public static final DeferredRegister<Potion> REGISTRY = DeferredRegister.create(
             ForgeRegistries.POTIONS, MoPotionsMod.MOD_ID);
 
-    public static final RegistryObject<Potion> HASTE_POTION        = REGISTRY.register(
-            "haste_potion",
+    /**
+     * Stores translations against the Item ID
+     */
+    public static final ArrayList<LangProvider.Translation<String>> TRANSLATIONS = new ArrayList<>();
+
+    public static final RegistryObject<Potion> HASTE_POTION        = register(
+            "haste_potion", "Haste",
             () -> new Potion(new MobEffectInstance(MobEffects.DIG_SPEED, 3600, 0, false, true)));
-    public static final RegistryObject<Potion> STRONG_HASTE_POTION = REGISTRY.register(
-            "strong_haste_potion",
+    public static final RegistryObject<Potion> STRONG_HASTE_POTION = register(
+            "strong_haste_potion", "Haste",
             () -> new Potion(new MobEffectInstance(MobEffects.DIG_SPEED, 1800, 1, false, true)));
-    public static final RegistryObject<Potion> LONG_HASTE_POTION   = REGISTRY.register(
-            "long_haste_potion",
+    public static final RegistryObject<Potion> LONG_HASTE_POTION   = register(
+            "long_haste_potion", "Haste",
             () -> new Potion(new MobEffectInstance(MobEffects.DIG_SPEED, 9600, 0, false, true)));
 
-    public static final RegistryObject<Potion> MINING_FATIGUE_POTION        = REGISTRY.register(
-            "mining_fatigue_potion",
+    public static final RegistryObject<Potion> MINING_FATIGUE_POTION        = register(
+            "mining_fatigue_potion", "Mining Fatigue",
             () -> new Potion(new MobEffectInstance(MobEffects.DIG_SLOWDOWN, 1800, 0, false, true)));
-    public static final RegistryObject<Potion> STRONG_MINING_FATIGUE_POTION = REGISTRY.register(
-            "strong_mining_fatigue_potion",
+    public static final RegistryObject<Potion> STRONG_MINING_FATIGUE_POTION = register(
+            "strong_mining_fatigue_potion", "Mining Fatigue",
             () -> new Potion(new MobEffectInstance(MobEffects.DIG_SLOWDOWN, 900, 1, false, true)));
-    public static final RegistryObject<Potion> LONG_MINING_FATIGUE_POTION   = REGISTRY.register(
-            "long_mining_fatigue_potion",
+    public static final RegistryObject<Potion> LONG_MINING_FATIGUE_POTION   = register(
+            "long_mining_fatigue_potion", "Mining Fatigue",
             () -> new Potion(new MobEffectInstance(MobEffects.DIG_SLOWDOWN, 4800, 0, false, true)));
 
-    public static final RegistryObject<Potion> NAUSEA      = REGISTRY.register(
-            "nausea",
+    public static final RegistryObject<Potion> NAUSEA      = register(
+            "nausea", "Nausea",
             () -> new Potion(new MobEffectInstance(MobEffects.CONFUSION, 1800, 0, false, true)));
-    public static final RegistryObject<Potion> LONG_NAUSEA = REGISTRY.register(
-            "long_nausea",
+    public static final RegistryObject<Potion> LONG_NAUSEA = register(
+            "long_nausea", "Nausea",
             () -> new Potion(new MobEffectInstance(MobEffects.CONFUSION, 4800, 0, false, true)));
 
-    public static final RegistryObject<Potion> BLINDNESS        = REGISTRY.register(
-            "blindness",
+    public static final RegistryObject<Potion> BLINDNESS        = register(
+            "blindness", "Blindness",
             () -> new Potion(new MobEffectInstance(MobEffects.BLINDNESS, 1800, 0, false, true)));
-    public static final RegistryObject<Potion> STRONG_BLINDNESS = REGISTRY.register(
-            "strong_blindness",
+    public static final RegistryObject<Potion> STRONG_BLINDNESS = register(
+            "strong_blindness", "Blindness",
             () -> new Potion(new MobEffectInstance(MobEffects.BLINDNESS, 500, 2, false, true)));
-    public static final RegistryObject<Potion> LONG_BLINDNESS   = REGISTRY.register(
-            "long_blindness",
+    public static final RegistryObject<Potion> LONG_BLINDNESS   = register(
+            "long_blindness", "Blindness",
             () -> new Potion(new MobEffectInstance(MobEffects.BLINDNESS, 4800, 0, false, true)));
 
-    public static final RegistryObject<Potion> DECAY        = REGISTRY.register(
-            "decay",
+    public static final RegistryObject<Potion> DECAY        = register(
+            "decay", "Decay",
             () -> new Potion(new MobEffectInstance(MobEffects.WITHER, 1800, 0, false, true)));
-    public static final RegistryObject<Potion> STRONG_DECAY = REGISTRY.register(
-            "strong_decay",
+    public static final RegistryObject<Potion> STRONG_DECAY = register(
+            "strong_decay", "Decay",
             () -> new Potion(new MobEffectInstance(MobEffects.WITHER, 900, 1, false, true)));
-    public static final RegistryObject<Potion> LONG_DECAY   = REGISTRY.register(
-            "long_decay",
+    public static final RegistryObject<Potion> LONG_DECAY   = register(
+            "long_decay", "Decay",
             () -> new Potion(new MobEffectInstance(MobEffects.WITHER, 4800, 0, false, true)));
 
-    public static final RegistryObject<Potion> RESISTANCE        = REGISTRY.register(
-            "resistance",
+    public static final RegistryObject<Potion> RESISTANCE        = register(
+            "resistance", "Resistance",
             () -> new Potion(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 3600, 0, false, true)));
-    public static final RegistryObject<Potion> LONG_RESISTANCE   = REGISTRY.register(
-            "long_resistance",
+    public static final RegistryObject<Potion> LONG_RESISTANCE   = register(
+            "long_resistance", "Resistance",
             () -> new Potion(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 9600, 0, false, true)));
-    public static final RegistryObject<Potion> STRONG_RESISTANCE = REGISTRY.register(
-            "strong_resistance",
+    public static final RegistryObject<Potion> STRONG_RESISTANCE = register(
+            "strong_resistance", "Resistance",
             () -> new Potion(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 1800, 1, false, true)));
 
-    public static final RegistryObject<Potion> GLOWING      = REGISTRY.register(
-            "glowing",
+    public static final RegistryObject<Potion> GLOWING      = register(
+            "glowing", "Glowing",
             () -> new Potion(new MobEffectInstance(MobEffects.GLOWING, 3600, 0, false, true)));
-    public static final RegistryObject<Potion> LONG_GLOWING = REGISTRY.register(
-            "long_glowing",
+    public static final RegistryObject<Potion> LONG_GLOWING = register(
+            "long_glowing", "Glowing",
             () -> new Potion(new MobEffectInstance(MobEffects.GLOWING, 9600, 0, false, true)));
 
-    public static final RegistryObject<Potion> LEVITATION        = REGISTRY.register(
-            "levitation",
+    public static final RegistryObject<Potion> LEVITATION        = register(
+            "levitation", "Levitation",
             () -> new Potion(new MobEffectInstance(MobEffects.LEVITATION, 600, 0, false, true)));
-    public static final RegistryObject<Potion> STRONG_LEVITATION = REGISTRY.register(
-            "strong_levitation",
+    public static final RegistryObject<Potion> STRONG_LEVITATION = register(
+            "strong_levitation", "Levitation",
             () -> new Potion(new MobEffectInstance(MobEffects.LEVITATION, 400, 1, false, true)));
-    public static final RegistryObject<Potion> LONG_LEVITATION   = REGISTRY.register(
-            "long_levitation",
+    public static final RegistryObject<Potion> LONG_LEVITATION   = register(
+            "long_levitation", "Levitation",
             () -> new Potion(new MobEffectInstance(MobEffects.LEVITATION, 900, 0, false, true)));
 
-    public static final RegistryObject<Potion> SATISFACTION_POTION = REGISTRY.register(
-            "satisfaction_potion",
+    public static final RegistryObject<Potion> SATISFACTION_POTION = register(
+            "satisfaction_potion", "Satisfaction",
             () -> new Potion(new MobEffectInstance(MoPotionsEffects.SATISFACTION.get(), 600, 0, false, true)));
-    public static final RegistryObject<Potion> LONG_SATISFACTION   = REGISTRY.register(
-            "long_satisfaction",
+    public static final RegistryObject<Potion> LONG_SATISFACTION   = register(
+            "long_satisfaction", "Satisfaction",
             () -> new Potion(new MobEffectInstance(MoPotionsEffects.SATISFACTION.get(), 1000, 0, false, true)));
 
-    public static final RegistryObject<Potion> HUNGER        = REGISTRY.register(
-            "hunger",
+    public static final RegistryObject<Potion> HUNGER        = register(
+            "hunger", "Hunger",
             () -> new Potion(new MobEffectInstance(MobEffects.HUNGER, 1800, 0, false, true)));
-    public static final RegistryObject<Potion> LONG_HUNGER   = REGISTRY.register(
-            "long_hunger",
+    public static final RegistryObject<Potion> LONG_HUNGER   = register(
+            "long_hunger", "Hunger",
             () -> new Potion(new MobEffectInstance(MobEffects.HUNGER, 4800, 0, false, true)));
-    public static final RegistryObject<Potion> STRONG_HUNGER = REGISTRY.register(
-            "strong_hunger",
+    public static final RegistryObject<Potion> STRONG_HUNGER = register(
+            "strong_hunger", "Hunger",
             () -> new Potion(new MobEffectInstance(MobEffects.HUNGER, 900, 1, false, true)));
 
-    public static final RegistryObject<Potion> ABSORPTION        = REGISTRY.register(
-            "absorption",
+    public static final RegistryObject<Potion> ABSORPTION        = register(
+            "absorption", "Absorption",
             () -> new Potion(new MobEffectInstance(MobEffects.ABSORPTION, 3600, 0, false, true)));
-    public static final RegistryObject<Potion> LONG_ABSORPTION   = REGISTRY.register(
-            "long_absorption",
+    public static final RegistryObject<Potion> LONG_ABSORPTION   = register(
+            "long_absorption", "Absorption",
             () -> new Potion(new MobEffectInstance(MobEffects.ABSORPTION, 9600, 0, false, true)));
-    public static final RegistryObject<Potion> STRONG_ABSORPTION = REGISTRY.register(
-            "strong_absorption",
+    public static final RegistryObject<Potion> STRONG_ABSORPTION = register(
+            "strong_absorption", "Absorption",
             () -> new Potion(new MobEffectInstance(MobEffects.ABSORPTION, 1800, 1, false, true)));
 
-    public static final RegistryObject<Potion> LUCK      = REGISTRY.register(
-            "luck",
+    public static final RegistryObject<Potion> LUCK      = register(
+            "luck", "Luck",
             () -> new Potion(new MobEffectInstance(MobEffects.LUCK, 6000, 0, false, true)));
-    public static final RegistryObject<Potion> LONG_LUCK = REGISTRY.register(
-            "long_luck",
+    public static final RegistryObject<Potion> LONG_LUCK = register(
+            "long_luck", "Luck",
             () -> new Potion(new MobEffectInstance(MobEffects.LUCK, 9000, 0, false, true)));
 
-    public static final RegistryObject<Potion> BAD_LUCK      = REGISTRY.register(
-            "bad_luck",
+    public static final RegistryObject<Potion> BAD_LUCK      = register(
+            "bad_luck", "Bad Luck",
             () -> new Potion(new MobEffectInstance(MobEffects.UNLUCK, 3000, 0, false, true)));
-    public static final RegistryObject<Potion> LONG_BAD_LUCK = REGISTRY.register(
-            "long_bad_luck",
+    public static final RegistryObject<Potion> LONG_BAD_LUCK = register(
+            "long_bad_luck", "Bad Luck",
             () -> new Potion(new MobEffectInstance(MobEffects.UNLUCK, 4500, 0, false, true)));
 
-    public static final RegistryObject<Potion> CONDUIT_POWER      = REGISTRY.register(
-            "conduit_power",
+    public static final RegistryObject<Potion> CONDUIT_POWER      = register(
+            "conduit_power", "Conduit Power",
             () -> new Potion(new MobEffectInstance(MobEffects.CONDUIT_POWER, 9600, 0, false, true)));
-    public static final RegistryObject<Potion> LONG_CONDUIT_POWER = REGISTRY.register(
-            "long_conduit_power",
+    public static final RegistryObject<Potion> LONG_CONDUIT_POWER = register(
+            "long_conduit_power", "Conduit Power",
             () -> new Potion(new MobEffectInstance(MobEffects.CONDUIT_POWER, 14400, 0, false, true)));
+
+    private static RegistryObject<Potion> register(String id, String effectName, Supplier<Potion> potion) {
+        TRANSLATIONS.add(new Translation<>(id, effectName));
+        return REGISTRY.register(id, potion);
+    }
 }
