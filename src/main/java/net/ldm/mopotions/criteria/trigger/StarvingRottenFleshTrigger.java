edf11b17cd0e9e43cef9e00da2f65ad8729fa40b
Logan Dhillon <dev@logandhillon.com>
2026-07-21 21:43:59 -0400

    feat: migrate criterion/triggers to neoforge

--- src/main/java/net/ldm/mopotions/criteria/trigger/StarvingRottenFleshTrigger.java
+++ src/main/java/net/ldm/mopotions/criteria/trigger/StarvingRottenFleshTrigger.java
@@ -1,43 +1,41 @@
 package net.ldm.mopotions.criteria.trigger;
 
-import com.google.gson.JsonObject;
-import net.minecraft.advancements.critereon.AbstractCriterionTriggerInstance;
+import com.mojang.serialization.Codec;
 import net.minecraft.advancements.critereon.ContextAwarePredicate;
-import net.minecraft.advancements.critereon.DeserializationContext;
+import net.minecraft.advancements.critereon.CriterionValidator;
 import net.minecraft.advancements.critereon.SimpleCriterionTrigger;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+
+import java.util.Optional;
 
 /**
  * @author Logan Dhillon
  */
 public class StarvingRottenFleshTrigger extends SimpleCriterionTrigger<StarvingRottenFleshTrigger.TriggerInstance> {
-    public static final ResourceLocation ID =
-            new ResourceLocation("mo_potions", "ate_rotten_flesh_while_starving");
 
     @Override
-    protected StarvingRottenFleshTrigger.TriggerInstance createInstance(JsonObject obj,
-                                                                        ContextAwarePredicate player,
-                                                                        DeserializationContext ctx) {
-        return new TriggerInstance(player);
+    public Codec<TriggerInstance> codec() {
+        return TriggerInstance.CODEC;
     }
 
-    @Override
-    public ResourceLocation getId() {
-        return ID;
+    public void trigger(ServerPlayer player) {
+        trigger(player, instance -> true);
     }
 
-    public void trigger(ServerPlayer pPlayer) {
-        trigger(pPlayer, instance -> true);
-    }
+    public static record TriggerInstance(
+            Optional<ContextAwarePredicate> player) implements SimpleCriterionTrigger.SimpleInstance {
+        public static final Codec<TriggerInstance> CODEC =
+                ContextAwarePredicate.CODEC.optionalFieldOf("player")
+                                           .xmap(TriggerInstance::new, TriggerInstance::player)
+                                           .codec();
 
-    public static class TriggerInstance extends AbstractCriterionTriggerInstance {
-        public TriggerInstance(ContextAwarePredicate player) {
-            super(ID, player);
+        @Override
+        public void validate(CriterionValidator validator) {
+            SimpleCriterionTrigger.SimpleInstance.super.validate(validator);
         }
 
         public static TriggerInstance instance() {
-            return new TriggerInstance(ContextAwarePredicate.ANY);
+            return new TriggerInstance(Optional.empty());
         }
     }
 }
